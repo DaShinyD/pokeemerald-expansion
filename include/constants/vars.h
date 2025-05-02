ce3ffe49cb5adf22d69957a2b2f34950c29caf35
DaShinyD <45526509+DaShinyD@users.noreply.github.com>
2025-05-01 20:27:18 -0500

    Update vars.h

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -239,8 +239,8 @@
 #define VAR_UNUSED_0x40DB                                0x40DB // Gamecorner
 #define VAR_UNUSED_0x40DC                                0x40DC // Gamecorner
 #define VAR_GIFT_PICHU_SLOT                              0x40DD
-#define VAR_GIFT_UNUSED_1                                0x40DE // DexNav
-#define VAR_GIFT_UNUSED_2                                0x40DF // DexNav
+#define VAR_GIFT_UNUSED_1                                0x40DE // Var is written to, but never read
+#define VAR_GIFT_UNUSED_2                                0x40DF // Var is written to, but never read
 #define VAR_GIFT_UNUSED_3                                0x40E0 // Var is written to, but never read
 #define VAR_GIFT_UNUSED_4                                0x40E1 // Var is written to, but never read
 #define VAR_GIFT_UNUSED_5                                0x40E2 // Var is written to, but never read
