890eeea9a33b3e9207dfdbf59326a1d61c35fb45
DaShinyD <45526509+DaShinyD@users.noreply.github.com>
2025-05-03 18:10:10 -0500

    mugshots for jokes

--- src/data/field_mugshots.h
+++ src/data/field_mugshots.h
@@ -2,6 +2,10 @@ static const u32 sFieldMugshotGfx_TestNormal[] = INCBIN_U32("graphics/field_mugs
 static const u32 sFieldMugshotGfx_TestAlt[] = INCBIN_U32("graphics/field_mugshots/test/alt.4bpp.lz");
 static const u16 sFieldMugshotPal_TestNormal[] = INCBIN_U16("graphics/field_mugshots/test/normal.gbapal");
 static const u16 sFieldMugshotPal_TestAlt[] = INCBIN_U16("graphics/field_mugshots/test/alt.gbapal");
+static const u32 sFieldMugshotGfx_Kemo[] = INCBIN_U32("graphics/field_mugshots/kemo/kemo.4bpp.lz");
+static const u16 sFieldMugshotPal_Kemo[] = INCBIN_U16("graphics/field_mugshots/kemo/kemo.gbapal");
+static const u32 sFieldMugshotGfx_Goku[] = INCBIN_U32("graphics/field_mugshots/goku/goku.4bpp.lz");
+static const u16 sFieldMugshotPal_Goku[] = INCBIN_U16("graphics/field_mugshots/goku/goku.gbapal");
 
 struct MugshotGfx
 {
@@ -23,6 +27,34 @@ static const struct MugshotGfx sFieldMugshots[MUGSHOT_COUNT][EMOTE_COUNT] =
         {
             .gfx = sFieldMugshotGfx_TestAlt,
             .pal = sFieldMugshotPal_TestAlt,
+        },
+    },
+    [MUGSHOT_KEMO] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Kemo,
+            .pal = sFieldMugshotPal_Kemo,
+        },
+
+        [EMOTE_ALT] =
+        {
+            .gfx = sFieldMugshotGfx_Kemo,
+            .pal = sFieldMugshotPal_Kemo,
+        },
+    },
+    [MUGSHOT_GOKU] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_Goku,
+            .pal = sFieldMugshotPal_Goku,
+        },
+
+        [EMOTE_ALT] =
+        {
+            .gfx = sFieldMugshotGfx_Goku,
+            .pal = sFieldMugshotPal_Goku,
         },
     },
 };
