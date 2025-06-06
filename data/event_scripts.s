a69aec703732e348698daa750f57f1feaf4b8bf0
DaShinyD <45526509+DaShinyD@users.noreply.github.com>
2025-06-06 13:05:32 -0500

    gbcbh

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1472,5 +1472,10 @@ MysteryGift_Text_ReceivedGiftMon:
 	.include "data/maps/UnknownRuins/scripts.inc"
 	
 	.include "data/scripts/dexnav.inc"
+	
+	.include "data/maps/UnknownRuinsGodCave/scripts.inc"
 
 	.include "data/maps/UnknownRuinsGodCave/scripts.inc"
+
+	.include "data/maps/BillsGarden/scripts.inc"
+
